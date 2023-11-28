31be37566582a1073a1db5f8a2d0c110bc778c56
ahm-fahim <contactahmfahim@gmail.com>
2023-11-28 19:42:42 +0600

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 // import { useState } from "react"
 import { logo } from '../assets'
-// import { navlinks } from '../constants'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="bank" className="w-[124px] h-[32px]" /> 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        
+        {navLinks.map}
       </ul>
 
     </nav>

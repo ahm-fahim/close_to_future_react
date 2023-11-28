ee8733e59b74a9fdac62cee4a744d29e217031ff
ahm-fahim <contactahmfahim@gmail.com>
2023-11-28 19:43:44 +0600

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,11 @@ const Navbar = () => {
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} alt="bank" className="w-[124px] h-[32px]" /> 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
-        {navLinks.map}
+        {navLinks.map((nav, idx) => (
+          <li
+            
+          ></li>
+        ))}
       </ul>
 
     </nav>
